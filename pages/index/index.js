7aa93aa66e0c48b6cb10cfb2c5d25a52928bc7be
zhujunkun <zhujunkun@yit.com>
2019-04-16 17:19:32 +0800

    添加tabbar底部跳转

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,10 +8,37 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    tabbarList: [
+      {
+        tabIndex: 0,
+        name: '首页',
+        url: '../index/index',
+        icon: 'home-o'
+      },
+      {
+        tabIndex: 1,
+        name: '搜索',
+        url: '../search/index',
+        icon: 'search'
+      },
+      {
+        tabIndex: 2,
+        name: '我的',
+        url: '../center/index',
+        icon: 'manager-o'
+      },
+    ]
   },
   onChange: function(e){
     console.log(e);
+    const { tabbarList } = this.data
+    wx.navigateTo({
+      url: tabbarList[e.detail].url,
+      success: ()=>{
+        // console.log('跳转成功');
+      }
+    })
   },
   //事件处理函数
   bindViewTap: function() {
@@ -26,7 +53,7 @@ Page({
         hasUserInfo: true
       })
     } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 由于 getUserInfo 是网络请求,可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
